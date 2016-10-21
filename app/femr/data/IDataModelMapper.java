b04d0e604497f40ede0d91137efb0c7f7759e62d
psusheelk <sxp155831@utdallas.edu>
2016-10-21 01:54:44 -0500

    CR-FEMR-SPMB-137
    CR-FEMR-SPMB-137

--- app/femr/data/IDataModelMapper.java
+++ app/femr/data/IDataModelMapper.java
@@ -139,13 +139,14 @@ public interface IDataModelMapper {
      * @param firstName first name of the patient, not null
      * @param lastName  last name of the patient, not null
      * @param birthday  the patients birthday, may be null
+     * @param fakeBDflag fake generated indicator
      * @param sex       the sex of the patient, may be null
      * @param address   the address of the patients residence, may be null
      * @param city      the city of the patient, may be null
      * @param photoID   the id of a photo of the patient, may be null
      * @return an implementation of IPatient or null if processing fails
      */
-    IPatient createPatient(int userID, String firstName, String lastName, Date birthday, String sex, String address, String city, Integer photoID);
+    IPatient createPatient(int userID, String firstName, String lastName, Date birthday,Integer fakeBDFlag, String sex, String address, String city, Integer photoID);
 
     /**
      * Generate and provide an implementation of IPatientEncounter.
